f96a25bbdfdc9262fdc829e6b798dc652b107e3b
devGiovanni95 <giovanni.devjava@hotmail.com>
2021-06-05 22:13:44 -0300

    Parte2

--- app-calculo/src/module-info.java
+++ app-calculo/src/module-info.java
@@ -1,4 +1,4 @@
-open module app.calculo {
+ module app.calculo {
 
     //utilizando o modo transitivo ele consegue pegar na classe do pai fazendo uma interação com logger mesmo nao importando a classe pois a classe calculo ja fez a chamada
     requires transitive app.logging;
@@ -6,4 +6,6 @@ open module app.calculo {
 
     exports br.com.giovanni.app.calculo.interno
             to app.financeiro;
+
+    opens br.com.giovanni.app.calculo;
 }
